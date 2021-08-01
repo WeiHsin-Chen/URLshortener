a7e17f142599d7eca6013dee05836167e3bfaa62
Wayne <urrightguy@gmail.com>
2021-08-01 11:48:48 +0800

    add background files

---
+++ app.js
@@ -0,0 +1,30 @@
+// include packages and define server related variables
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const routes = require('./routes')
+
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files for css
+app.use(express.static('public'))
+
+// setting body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// setting mongoose
+require('./config/mongoose')
+
+// 引用 routes
+app.use(routes)
+
+//start and listen on the Express server
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
