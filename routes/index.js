67b54948e48742265594484fc179de7a1de83a9b
Wayne <urrightguy@gmail.com>
2021-08-01 12:12:02 +0800

    add home view

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@
 const express = require('express')
 const router = express.Router()
 
-// // 引入 home 模組程式碼
-// const home = require('./modules/home')
-// // 將網址結構符合 / 字串的 request 導向 home 模組 
-// router.use('/', home)
+// 引入 home 模組程式碼
+const home = require('./modules/home')
+// 將網址結構符合 / 字串的 request 導向 home 模組 
+router.use('/', home)
 
 // // 引入 restaurants 模組程式碼
 // const booking = require('./modules/booking')
---
+++ routes/modules/home.js
@@ -0,0 +1,12 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+
+// route setting with models seeder connection
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+// 匯出路由模組
+module.exports = router
