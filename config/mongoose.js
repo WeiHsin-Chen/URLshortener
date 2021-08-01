619c78c0da6d10168a28396fe7270865adf87050
Wayne <urrightguy@gmail.com>
2021-08-01 11:58:50 +0800

    update mongoose.js

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/record-list'
+const db = mongoose.connection
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+module.exports = db
